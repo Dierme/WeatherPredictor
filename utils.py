04df4e7b3fd7fa44b1f63f785d740b1ae1165395
Oleksii <kalimdor07@gmail.com>
2018-03-11 23:19:50 +0100

    get month by unix

--- utils.py
+++ utils.py
@@ -5,3 +5,9 @@ def unix_to_datetime(unix):
     return datetime.datetime.fromtimestamp(
             int(unix)
         ).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def get_month_by_dt(unix):
+    return datetime.datetime.fromtimestamp(
+        int(unix)
+    ).strftime('%m')
